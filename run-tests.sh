9ed8409aedfabac6ec65e27b26690705180e304b
Ryan Kennedy <ryan5544@gmail.com>
2013-03-02 19:38:49 -0500

    Added script to run tests under tests/auto.

---
+++ run-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+for test_case in `find ./tests/auto -type f`; do
+    compiler_output=`./compiler $test_case`
+    test_case=`echo $test_case | sed 's/\.\/tests\/auto\///'`
+    if [[ "$compiler_output" != "" ]]; then
+        echo -e "$test_case" '\x1b[31mCompile Failed\x1b[0m'
+    else
+        test_output=`./output`
+        if [[ "$test_output" = "PASS" ]]; then
+            echo -e "$test_case" '\x1b[32mPASS\x1b[0m'
+        elif [[ "$test_output" = "FAIL" ]]; then
+            echo -e "$test_case" '\x1b[31mFAIL\x1b[0m'
+        else
+            echo -e "$test_case" '\x1b[31mMalformed Output\x1b[0m'
+        fi
+    fi
+done
